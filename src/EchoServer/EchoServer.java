3f5f7e74be2c9710f9b494778f1ecb7d0d6c3a57
Mikolaj2400 <mikka2000@interia.pl>
2022-05-18 06:40:07 +0200

    Minor improvements

--- src/EchoServer/EchoServer.java
+++ src/EchoServer/EchoServer.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package EchoServer;
 
-/**
- *
- * @author dzelazny
- */
 import java.net.*;
 import java.io.*;
 
